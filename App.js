6c151bcafe313456c29ade8d0e52b6756bda4139
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-09-15 18:16:03 +0300

    Update App

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
 import { StyleSheet, Text, View } from 'react-native';
+// import RegistrationScreen from './src/Screens/RegistrationScreen';
+import LoginScreen from './src/Screens/LoginScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    const [fontsLoaded] = useFonts({
+        "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+        "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+        "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    });
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
+
+    return (
+    <View style={styles.flexContainer}>
       <StatusBar style="auto" />
+      {/*<RegistrationScreen />*/}
+      <LoginScreen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  flexContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
